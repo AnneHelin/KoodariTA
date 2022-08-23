81515028c70aef861bd5e527795b2ddc1c5d33e3
AnneHelin <43341@edu.raseko.fi>
2022-08-23 14:51:41 +0300

    Added a funtion

---
+++ app.py
@@ -0,0 +1,25 @@
+# MAIN APPLICATION 
+
+# Ask user name
+firstName = 'Anne'
+likeCats = input('Pidätkö kissoista? (k/e)')
+print('Morjensta', firstName)
+
+if likeCats == 'k':
+    print('Olet kissaihminen')
+    
+def nameLength(name):
+    """Calculates lenght of the name
+
+    Args:
+        name (str): name of the person
+
+    Returns:
+        int: lenght of the name
+    """
+    lenght = len(name)
+    return lenght  
+
+# Calculate name lenght
+length = nameLength(firstName)
+print(firstName, 'snanssa on', length, 'kirjainta')
