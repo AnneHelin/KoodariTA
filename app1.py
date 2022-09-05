7a6afe709b61d46fc0405ad1fee1e8807e58a391
AnneHelin <43341@edu.raseko.fi>
2022-09-05 09:08:11 +0300

    Lisätty koodia

---
+++ app1.py
@@ -0,0 +1,13 @@
+# SOVELLUKSEN PÄÄOHJELMA
+
+# Kirjastojen ja modulien lataus
+from calendar import c
+import datetime
+from inspect 
+
+# Luokka-määritykset
+
+class Person():
+    """dostring for Classname"""
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
